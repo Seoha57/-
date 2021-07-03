3be3807f2f4b4208d7f0f07c3fde4ca566ce62f2
Seonghak Kim <55680794+Seoha57@users.noreply.github.com>
2021-07-03 17:25:10 +0900

    Add files via upload

---
+++ (11057)오르막 수.cpp
@@ -0,0 +1,56 @@
+/**************************************
+오르막 수
+시간 제한   메모리 제한
+   1 초        256 MB
+
+문제
+오르막 수는 수의 자리가 오름차순을 이루는 수를 말한다. 이때, 인접한 수가 같아도 오름차순으로 친다.
+예를 들어, 2234와 3678, 11119는 오르막 수이지만, 2232, 3676, 91111은 오르막 수가 아니다.
+수의 길이 N이 주어졌을 때, 오르막 수의 개수를 구하는 프로그램을 작성하시오. 수는 0으로 시작할 수 있다.
+
+입력
+첫째 줄에 N (1 ≤ N ≤ 1,000)이 주어진다.
+
+출력
+첫째 줄에 길이가 N인 오르막 수의 개수를 10,007로 나눈 나머지를 출력한다.
+**************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n) {
+	vector<vector<int>> t(n + 1, vector<int>(10, 0));
+
+	for (int i = 0; i < 10; ++i) {
+		t[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			for (int k = 0; k <= j; ++k) {
+				t[i][j] += t[i - 1][k];
+			}
+			t[i][j] %= 10007;
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < 10; ++i) {
+		ans += t[n][i];
+	}
+	return ans % 10007;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	cout << solution(n) << '\n';
+
+	return 0;
+}
